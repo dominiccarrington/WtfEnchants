9b933a0429538e42f099360ab5c67536316933ea
Dominic Carrington <carringtondominic@gmail.com>
2018-05-12 16:36:45 +0100

    Envenomation has a 50% of working

--- src/main/java/com/ragegamingpe/wtfenchants/common/enchantment/EnvenomationEnchantment.java
+++ src/main/java/com/ragegamingpe/wtfenchants/common/enchantment/EnvenomationEnchantment.java
@@ -7,8 +7,11 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.MobEffects;
 import net.minecraft.potion.PotionEffect;
 
+import java.util.Random;
+
 public class EnvenomationEnchantment extends ModBaseEnchantment
 {
+    private static Random rand;
     public EnvenomationEnchantment()
     {
         super("envenomation", Rarity.RARE, EnumEnchantmentType.WEAPON);
@@ -17,7 +20,9 @@ public class EnvenomationEnchantment extends ModBaseEnchantment
     @Override
     public void onEntityDamaged(EntityLivingBase user, Entity target, int level)
     {
-        if (target instanceof EntityLivingBase) {
+        if (rand == null) rand = new Random();
+
+        if (rand.nextFloat() < 0.5 && target instanceof EntityLivingBase) {
             ((EntityLivingBase) target).addPotionEffect(new PotionEffect(MobEffects.POISON, 100, level - 1));
         }
     }
