20a303d21aa9dc18244f276340d69b99fdfcc9f3
Dominic Carrington <carringtondominic@gmail.com>
2018-05-27 21:05:25 +0100

    Pick Block works for bookshelves

--- src/main/java/com/ragegamingpe/wtfenchants/common/block/BlockBookshelf.java
+++ src/main/java/com/ragegamingpe/wtfenchants/common/block/BlockBookshelf.java
@@ -16,6 +16,7 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
@@ -26,6 +27,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.property.ExtendedBlockState;
@@ -38,6 +40,9 @@ import net.minecraftforge.oredict.OreDictionary;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+/**
+ * FIX: PICK BLOCK
+ */
 public class BlockBookshelf extends ModBlockInventory
 {
     public static final PropertyUnlistedInteger BOOKS = new PropertyUnlistedInteger("books", 0, 14);
@@ -148,6 +153,31 @@ public class BlockBookshelf extends ModBlockInventory
                 list.add(createItemstack(this, 0, block, blockMeta));
             }
         }
+    }
+
+    @Override
+    public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player)
+    {
+        ItemStack item = new ItemStack(this, 1);
+        TileEntity te = world.getTileEntity(pos);
+
+        if (te instanceof TileEntityBookshelf) {
+            TileEntityBookshelf bookshelf = (TileEntityBookshelf) te;
+
+            NBTTagCompound tag = NBTHelper.getTagSafe(item);
+
+            // texture
+            NBTTagCompound data = bookshelf.getTextureBlock();
+
+            if (!data.hasNoTags()) {
+                tag.setTag(TileEntityBookshelf.PLANKS_TAG, data);
+                item.setTagCompound(tag);
+            }
+
+            teStore = null;
+        }
+
+        return item;
     }
 
     @Override
